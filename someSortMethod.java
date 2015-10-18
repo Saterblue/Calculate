9aefa376b421f47fabe5ee609041ea159887c537
Saterblue <Saterblue@users.noreply.github.com>
2015-10-18 14:19:55 +0800

    Update and rename samSortMethod.java to someSortMethod.java

--- someSortMethod.java
+++ someSortMethod.java
@@ -1,16 +1,16 @@
   //the InserSort method
    public static void insertSort(int[] arr) {
-			int sign;
-			for(int i=1;i<arr.length;i++) {
+	int sign;
+	for(int i=1;i<arr.length;i++) {
 		//Set temp to mark the current element 
-				int temp=arr[i];
-				sign=i;
+		int temp=arr[i];
+		sign=i;
 		//if temp is less than the prevois elements,temp's sit give the elements,until temp find the sit 
-				while(sign>0&&arr[sign-1]>temp) {
-					arr[sign]=arr[sign-1];
-					sign--;
-				}
+		while(sign>0&&arr[sign-1]>temp) {
+		arr[sign]=arr[sign-1];
+			sign--;
+		}
 		//temp find the sit,and sitdown
-				arr[sign]=temp;
-			}
+		arr[sign]=temp;
+	}
    }
